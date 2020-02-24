57260749a5a45e9039fb3ab9bb111913236cd93d
learngolangwithpalakala <60282319+learngolangwithpalakala@users.noreply.github.com>
2020-02-24 20:23:24 +0100

    Update mysql_dbscript.sql

--- mysql_dbscript.sql
+++ mysql_dbscript.sql
@@ -1,21 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `thools` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `thools`;
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: localhost    Database: article
--- ------------------------------------------------------
--- Server version	5.7.18
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE  IF NOT EXISTS `myschema` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `myschema`;
 
 --
 -- Table structure for table `user`
@@ -261,5 +245,5 @@ GRANT ALL PRIVILEGES ON thools.* TO  'admin'@'%';
 
 
 
-insert into thools.role(role) values ('ADMIN');
-insert into thools.role(role) values ('USER');
+insert into myschema.role(role) values ('ADMIN');
+insert into myschema.role(role) values ('USER');
